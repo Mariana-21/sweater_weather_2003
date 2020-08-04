76b8381592f223de0ab8fe17cbd37570cccb8e75
Mariana Cid <cidmariana5@gmail.com>
2020-08-04 09:39:54 -0600

    added to the test expect json response

--- spec/requests/api/v1/final_retake_munchies_spec.rb
+++ spec/requests/api/v1/final_retake_munchies_spec.rb
@@ -5,5 +5,7 @@ RSpec.describe "Munchies api endpoint" do
     get '/api/v1/munchies?start=denver,co&end=pueblo,co&food=chinese'
 
     expect(response).to be_successful
+
+    json = JSON.parse(response.body, symbolize_names: true)
   end
 end
