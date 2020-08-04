6e8ec3c7710daf77f8cb9a46135884d56326eaab
Mariana Cid <cidmariana5@gmail.com>
2020-08-04 13:12:07 -0600

    changed to munchie poro

--- app/services/yelp_service.rb
+++ app/services/yelp_service.rb
@@ -6,6 +6,7 @@ class YelpService
       r.params[:term] = food_type
       r.params[:latitude] = lat
       r.params[:longitude] = long
+      r.params[:limit] = 1
     end
 
     json = JSON.parse(response.body, symbolize_names: true)
@@ -13,13 +14,13 @@ class YelpService
 
   def rest_object(lat, lon, food_type)
     rest_data = get_restaurants_by_end_location(lat,lon, food_type)
-    RestaurantFacade.new(rest_data)
+    MunchieResults.new(rest_data)
   end
-  
+
   private
     def conn
       Faraday.new('https://api.yelp.com/v3') do |f|
-        f.header[:Authorization] = ENV['YELP_KEY']
+        f.headers[:Authorization] = "Bearer #{ENV['YELP_KEY']}"
       end
     end
 end
