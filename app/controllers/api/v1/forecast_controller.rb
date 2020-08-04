4520bfc71d44aa0b672772061d51461e15c18abd
Mariana Cid <cidmariana5@gmail.com>
2020-08-04 09:11:41 -0600

    new lat and long info

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,9 +1,19 @@
 class Api::V1::ForecastController < ApplicationController
   def index
     location = LocationResults.new.get_lat_long_by_city(params[:location])
+    lat = location.first
+    long = location.last
     # binding.pry
-    weather_data = WeatherResults.new.weather_by_location(location)   
+    weather_data = WeatherResults.new.weather_by_location(lat, long) 
+    # binding.pry  
 
-    render json: ForecastSerializer.new(weather_data).serialized_json
+    render json: ForecastSerializer.new(weather_data)
   end
+
+  # private
+
+  #   def get_forecast_object
+  #     forecast_service = WeatherService.new(params[:location])
+  #     forecast_service.weather_objects(lat, long)
+  #   end
 end
