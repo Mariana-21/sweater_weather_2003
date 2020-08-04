6ba4eb450f448b89fa4b53164c9709392567e8e3
Mariana Cid <cidmariana5@gmail.com>
2020-08-03 23:41:42 -0600

    new location objects

--- app/services/geocoding_service.rb
+++ app/services/geocoding_service.rb
@@ -1,10 +1,16 @@
 class GeocodingService
-  def location_lat_lon(location)
+  def location_lat_long(location)
     response = conn.get('/geocoding/v1/address') do |r|
       r.params[:location] = location
     end
     JSON.parse(response.body, symbolize_names: true)
   end
+
+  def get_location_objects(location)
+    location_info = location_lat_long(location)
+    Geocode.new(location_info)
+  end
+  
   private
     def conn
       Faraday.new('http://www.mapquestapi.com') do |f|
