2f95cd9bfeacf0195d7672c7542d4b643f4aea39
Mariana Cid <cidmariana5@gmail.com>
2020-08-04 10:16:34 -0600

    location added

--- app/controllers/api/v1/munchies_controller.rb
+++ app/controllers/api/v1/munchies_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::MunchiesController < ApplicationController
   def index
     binding.pry
+    location = LocationResults.new.get_lat_long_by_city(params[:location])
     render json: MunchieSerializer.new
   end
 end
