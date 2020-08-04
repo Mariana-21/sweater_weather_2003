6c9ab8fe1327a2f37fce14bcc8a34e2cfe2eb755
Mariana Cid <cidmariana5@gmail.com>
2020-08-04 16:18:16 -0600

    background image controller

--- app/controllers/api/v1/backgrounds_controller.rb
+++ app/controllers/api/v1/backgrounds_controller.rb
@@ -1,5 +1,12 @@
 class Api::V1::BackgroundsController < ApplicationController
   def index 
-    render json: BackgroundImageSerializer.new
+    render json: BackgroundImageSerializer.new(get_background_image)
   end
+
+  private
+
+    def get_background_image
+      location = params[:location]
+      (UnsplashService.new).get_image(location)
+    end
 end
