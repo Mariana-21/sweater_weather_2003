a590c1b5323267ca316fafaa7b1f25517b14b4a7
Mariana Cid <cidmariana5@gmail.com>
2020-08-04 10:16:22 -0600

    connection to yelp api created

--- app/services/yelp_service.rb
+++ app/services/yelp_service.rb
@@ -1,3 +1,19 @@
 class YelpService
-  
+
+  def get_restaurants_by_end_location(lat, long, food_type)
+    # lat_and_long = LocationResults.get_lat_long_by_city(end_location)
+    response = conn.get("/v3/businesses/search") do |r|
+      r.params[:term] = food_type
+      r.params[:latitude] = lat
+      r.params[:longitude] = long
+    end
+
+    json = JSON.parse(response.body, symbolize_names: true)
+  end
+  private
+    def conn
+      Faraday.new('https://api.yelp.com/v3') do |f|
+        f.header[:Authorization] = ENV['YELP_KEY']
+      end
+    end
 end
