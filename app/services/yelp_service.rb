e7eae974583c406189af8d997e0bc8a2f715c581
Mariana Cid <cidmariana5@gmail.com>
2020-08-04 10:25:11 -0600

    rest object created

--- app/services/yelp_service.rb
+++ app/services/yelp_service.rb
@@ -10,6 +10,12 @@ class YelpService
 
     json = JSON.parse(response.body, symbolize_names: true)
   end
+
+  def rest_object(lat, lon, food_type)
+    rest_data = get_restaurants_by_end_location(lat,lon, food_type)
+    RestaurantFacade.new(rest_data)
+  end
+  
   private
     def conn
       Faraday.new('https://api.yelp.com/v3') do |f|
