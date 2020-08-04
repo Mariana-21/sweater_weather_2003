43090b5692932fc7888cbb0b908d8bc08ff09c42
Mariana Cid <cidmariana5@gmail.com>
2020-08-04 10:26:09 -0600

    refactor

--- app/controllers/api/v1/munchies_controller.rb
+++ app/controllers/api/v1/munchies_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::MunchiesController < ApplicationController
   def index
+    location = LocationResults.new.get_lat_long_by_city(params[:end])
     binding.pry
-    location = LocationResults.new.get_lat_long_by_city(params[:location])
     render json: MunchieSerializer.new
   end
 end
