282fde25dd5332472aba63ff09a5b2fa9eb8241a
Mariana Cid <cidmariana5@gmail.com>
2020-08-03 23:43:01 -0600

    forecast controller

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,6 +1,9 @@
 class Api::V1::ForecastController < ApplicationController
   def index
+    location = LocationResults.new.get_lat_long_by_city(params[:location])
     # binding.pry
-    # render json: Forecast.new(params)
+    weather_data = WeatherResults.new.weather_by_location(location)   
+
+    render json: ForecastSerializer.new(weather_data).serialized_json
   end
 end
