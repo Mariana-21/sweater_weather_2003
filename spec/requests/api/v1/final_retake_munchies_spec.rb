54d2ac32d4715dba720aef0f7c63901aa940030c
Mariana Cid <cidmariana5@gmail.com>
2020-08-04 13:12:18 -0600

    added to test

--- spec/requests/api/v1/final_retake_munchies_spec.rb
+++ spec/requests/api/v1/final_retake_munchies_spec.rb
@@ -7,5 +7,17 @@ RSpec.describe "Munchies api endpoint" do
     expect(response).to be_successful
 
     json = JSON.parse(response.body, symbolize_names: true)
+
+    expected = {
+      :null=>"null",
+      :start_location=>"Denver",
+      :end_location=>"Pueblo",
+      :destination_forecast=>{:current=>"Clear", :arrival=>"Clear"},
+      :restaurant=>{:name=>"Kan's Kitchen", :address=>"1620 S Prairie Ave"}
+    }
+
+    expect(json).to eq(expected)
+    expect(json[:keys]).to eq([:null, :start_location, :end_location, :destination_forecast, :restaurant])  
+    binding.pry
   end
 end
