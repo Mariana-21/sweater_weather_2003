66ba812171c89e3bde2b3de19705691f165663f3
Mariana Cid <cidmariana5@gmail.com>
2020-08-03 23:41:09 -0600

    refactor

--- app/services/weather_service.rb
+++ app/services/weather_service.rb
@@ -1,14 +1,23 @@
 class WeatherService
+
+  def initialize
+    # @map_data = MapService.new.location_lat_long(@location)
+  end
+
   def location_weather(lat, lon)
     response = conn.get("/data/2.5/onecall") do |r|
       r.params[:lat] = lat 
       r.params[:lon] = lon
-      r.params[:exclude] = "minuetly"
       r.params[:units] = "imperial"
+      r.params[:exclude] = "minutely"
     end 
   json = JSON.parse(response.body, symbolize_names: true)
   end
 
+  def weather_objects(lat, lon)
+    # weather_data = location_weather(lat,lon)
+    WeatherResults.new.weather_by_location(lat, lon)
+  end
   private 
   
   def conn
@@ -16,4 +25,5 @@ class WeatherService
       faraday.params[:appid] = ENV['WEATHER_API_KEY']
     end
   end
+
 end
