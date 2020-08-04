5dbaa60fb25f8847cdd948f472c42f1cbc8c6b4c
Mariana Cid <cidmariana5@gmail.com>
2020-08-04 09:10:24 -0600

    change results

--- spec/poros/weather_results_spec.rb
+++ spec/poros/weather_results_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe WeatherResults do
     location_results = LocationResults.new.get_lat_long_by_city(location)
     lat = location_results.first
     long = location_results.last
-    results = WeatherResults.new.weather_by_location(lat, long)
+    results = WeatherResults.new.weather_by_location(location)
     binding.pry
   end
 end
