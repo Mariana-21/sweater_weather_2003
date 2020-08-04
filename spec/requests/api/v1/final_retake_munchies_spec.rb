d1664f5e9e54836b812883813c17cad7abb6b187
Mariana Cid <cidmariana5@gmail.com>
2020-08-04 09:28:56 -0600

    basic test created

---
+++ spec/requests/api/v1/final_retake_munchies_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe "Munchies api endpoint" do
+  it 'can give back all info needed for that location' do
+    get '/api/v1/munchies?start=denver,co&end=pueblo,co&food=chinese'
+
+    expect(response).to be_successful
+  end
+end
