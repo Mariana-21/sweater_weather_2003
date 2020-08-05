c91abcba82b92967c6fb129ea8eafa809dddecce
Mariana Cid <cidmariana5@gmail.com>
2020-08-05 01:15:24 -0600

    taking out commented out notes

--- app/controllers/api/v1/munchies_controller.rb
+++ app/controllers/api/v1/munchies_controller.rb
@@ -8,7 +8,7 @@ class Api::V1::MunchiesController < ApplicationController
     lat = end_location.first
     long = end_location.last
     munchie_data = MunchieResults.new.info(lat, long, food_type, start_lat, start_long)
-    # binding.pry
+
     render json: MunchieSerializer.new(munchie_data)
   end
 end
