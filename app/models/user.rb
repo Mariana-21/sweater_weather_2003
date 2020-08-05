025f4687292c1a88af095f6d6970414f9d8ce00d
Mariana Cid <cidmariana5@gmail.com>
2020-08-04 18:37:53 -0600

    user model created

---
+++ app/models/user.rb
@@ -0,0 +1,21 @@
+class User < ApplicationRecord
+  before_create :set_unique_api_key
+
+  validates :email, uniqueness: true, presence: true
+  validates :password_confirmation, presence: true
+  validates :api_key, uniqueness: true
+  has_secure_password
+
+  private
+
+    def set_unique_api_key
+      self.api_key = generate_key
+    end
+
+    def generate_key
+      loop do
+        key = SecureRandom.hex(10)
+        break key unless User.where(api_key: key).exists?
+      end
+    end
+end
