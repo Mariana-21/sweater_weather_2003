8e4cb40c5d87d49ecee1844bd24d585891358df1
Mariana Cid <cidmariana5@gmail.com>
2020-08-04 18:38:08 -0600

    user controller created

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,18 @@
+class Api::V1::UsersController < ApplicationController
+
+  def create 
+    # binding.pry
+    user = User.create(user_params)
+    if user.save
+      render json: UsersSerializer.new(user), status: 201
+    else
+      render json: { errors: user.errors.full_messages }, status: 420
+    end
+  end
+
+  private
+
+    def user_params
+      params.permit(:email, :password, :password_confirmation)
+    end
+end
